d43c7de14e792d204d0278051f47f89938a17bcb
Joyal Thomas <joyal@neoito.com>
2022-04-13 20:21:13 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import layout from './layout'
+export default function App() {
+  return (
+    <div>
+      <layout/>
+    </div>
+  )
+}
---
+++ src/layout.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// import env from 'env'
+export const layout = () => {
+  return (
+   <p>Hello from layout </p>
+  )
+}
+export default layout
